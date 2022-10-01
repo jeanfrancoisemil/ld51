e850b41acd435f6521032b0d9f1813fb4ce1b7cc
herbyvor <50072047+jeanfrancoisemil@users.noreply.github.com>
2022-10-01 23:27:51 +0100

    BackGround tilemap

--- Assets/Scripts/Timer.cs
+++ Assets/Scripts/Timer.cs
@@ -56,8 +56,8 @@ public class Timer : MonoBehaviour
 
     void DisplayTime(float timeToDisplay)
     {
-        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        
         float seconds = Mathf.FloorToInt(timeToDisplay % 60);
-        timeText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+        timeText.text = string.Format("{0:00}", seconds);
     }
 }
