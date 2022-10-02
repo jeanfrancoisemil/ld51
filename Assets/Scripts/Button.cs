db2db652d1913fe1790134b8709b182023fa8e8c
FirePh0enix <leonard@delbecq.net>
2022-10-02 12:01:04 +0200

    Added ``Button`` and ``Door`` prefabs

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -4,11 +4,14 @@ using UnityEngine;
 
 public class Button : MonoBehaviour
 {
+    private AudioSource clickSound;
     [HideInInspector]
     public bool isPressed = false;
 
     void Start()
     {
+        clickSound = GetComponent<AudioSource>();
+        clickSound.Pause();
     }
 
     void Update()
@@ -18,6 +21,8 @@ public class Button : MonoBehaviour
     private void OnTriggerEnter2D(Collider2D collision)
     {
         isPressed = true;
+        Debug.Log("dededde");
+        clickSound.Play();
     }
 
     private void OnTriggerExit2D(Collider2D collision)
