3fe8ae126151ff2db6178b605593000a1de41b8d
FirePh0enix <leonard@delbecq.net>
2022-10-03 10:36:34 +0200

    Added timer to doors

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -8,9 +8,13 @@ public class Door : MonoBehaviour
     public bool stayOpen = false;
     public bool needAllButtons = true;
     public bool defaultOpen = false;
+    public bool resetButtonsAfterTime = false;
+    public float resetTime = 0;
 
     [HideInInspector]
     public bool isOpen = false;
+    private float timer = 0;
+    private bool timerStarted = false;
 
     void Start()
     {
@@ -25,11 +29,30 @@ public class Door : MonoBehaviour
 
         var collider = GetComponent<BoxCollider2D>();
 
+        if (timerStarted && timer > 0)
+        {
+            timer -= Time.deltaTime;
+            Debug.Log(timer);
+        }
+        else if (timerStarted)
+        {
+            foreach (var button in buttons)
+                button.isPressed = false;
+            timerStarted = false;
+        }
+
         if (IsDoorOpen())
         {
             collider.enabled = false;
             GetComponent<SpriteRenderer>().enabled = false;
             isOpen = true;
+
+            // Demarrer le timer
+            if (resetButtonsAfterTime && !timerStarted)
+            {
+                timer = resetTime;
+                timerStarted = true;
+            }
         }
         else
         {
